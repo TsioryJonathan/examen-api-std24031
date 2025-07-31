658c7fcc372ecd975b1f13c74572612909be0a74
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-07-31 08:19:23 +0300

    feat: GET /ping which return pong

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 from pydantic import BaseModel
 from typing import List
 from starlette.requests import Request
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return JSONResponse(content={"message": "Hello, World!"}, status_code=200)
+@app.get("/ping")
+def read_ping():
+    return Response(content="pong", media_type="text/plain",status_code=200)
 
-class User(BaseModel):
-    name:str
-    age:int
+# class User(BaseModel):
+#     name:str
+#     age:int
 
-users_list: List[User] = []
+# users_list: List[User] = [{"name": "Alice", "age": 30}, {"name": "Bob", "age": 25}]
 
-def serialized_stored_user():
-    users_converted = []
-    for user in users_list:
-        users_converted.append(user.model_dump())
-    return users_converted
+# def serialized_stored_user():
+#     users_converted = []
+#     for user in users_list:
+#         users_converted.append(user.model_dump())
+#     return users_converted
 
-@app.post("/user")
-def postUser(user: User, request: Request):
-    accept_headers = request.headers.get("Accept")
-    if accept_headers != "text/plain":
-        return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
-    return JSONResponse({"User": user.model_dump()}, status_code=200)
+# @app.post("/user")
+# def postUser(user: User, request: Request):
+#     accept_headers = request.headers.get("Accept")
+#     if accept_headers != "text/plain":
+#         return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
+#     return JSONResponse({"User": user.model_dump()}, status_code=200)
