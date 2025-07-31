f2176918878616b963d1fcf94c947184ba228e47
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-07-31 08:26:06 +0300

    feat: 404 not found page for non existing routes

--- main.py
+++ main.py
@@ -19,6 +19,18 @@ def read_home():
         status_code=200,
         media_type="text/html"
     )
+    
+    
+@app.get("/{full_path:path}")
+def catch_all(full_path: str):
+    with open ("404.html" , "r" , encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(
+        content=html_content,
+        status_code=404,
+        media_type="text/html"
+    )
+
 # class User(BaseModel):
 #     name:str
 #     age:int
